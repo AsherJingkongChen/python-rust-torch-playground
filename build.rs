3ccca1cfcb82924c4bba9168f18dc44fe69970c4
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-03-26 02:17:52 +0800

    (ci-fix): Cannot use feature label on stable release mode

--- build.rs
+++ build.rs
@@ -1,9 +1,5 @@
-#![feature(cfg_match)]
-
 fn main() {
-    cfg_match! {
-        cfg(target_family = "unix") => {
-            println!(r"cargo:rustc-link-search=native=/usr/lib");
-        }
+    if cfg!(target_family = "unix") {
+        println!(r"cargo:rustc-link-search=native=/usr/lib");
     }
 }
